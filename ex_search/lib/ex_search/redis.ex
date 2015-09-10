6b4d15e46123dce0f783a5460848f436a28ad6f4
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2015-09-11 02:50:03 +0300

    Imports are lexical

--- ex_search/lib/ex_search/redis.ex
+++ ex_search/lib/ex_search/redis.ex
@@ -1,6 +1,4 @@
 defmodule ExSearch.Redis do
-  import Exredis.Api
-
   def start do
     {:ok, redis} = Exredis.start_link
     redis
@@ -10,6 +8,7 @@ defmodule ExSearch.Redis do
   @content "c:"
 
   def find_docs(words) do
+    import Exredis.Api
     redis = defaultclient # where am I supposed to store it?
     redis |> sinter(Enum.map(words, fn(str) -> @token <> String.downcase(str) end))
     |> Enum.map(fn(id) -> { id, redis |> get(@content <> id) } end)
